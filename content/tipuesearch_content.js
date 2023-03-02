2539a0b8641a2daa88ef8cedf0a263ff63b6a020
jason60714 <jason910807@gmail.com>
2023-03-02 15:56:59 +0800

    test 40923115

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n \n \n test \n', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n \n \n test \n test 40923115', 'tags': '', 'url': 'About.html'}]};
